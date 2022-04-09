a9f0c7e8c4a9fa08bfd83f5a60e48d12d7986b8e
iykjp <yukiwork.125@gmail.com>
2022-04-09 14:19:17 +0900

    add GA

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,35 @@ import '../styles/global.css'
 import '../styles/HoliTheme.css'
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import GoogleAnalytics from '@/components/GoogleAnalytics'
+import { useRouter } from "next/router";
+import React, { useEffect } from 'react'
 
+import { existsGaId, pageview } from '../lib/gtag'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!existsGaId) {
+      return
+    }
+
+    const handleRouteChange = (path) => {
+      pageview(path)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return(
     <>
       <Layout>
+        <GoogleAnalytics />
         <Component {...pageProps} />
       </Layout>
     </>
