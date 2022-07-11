3906b37c28e6321116f456900dce022098cc7178
lazurite2 <yukiwork.125@gmail.com>
2022-07-12 00:09:45 +0900

    deltete togglecomponent

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,3 @@
-import Link from 'next/link';
-import ToggleTheme from './ToggleTheme';
-
 export default function Header() {
     return (
             <div className="navbar px-0 bg-base-100">
@@ -8,11 +5,6 @@ export default function Header() {
                     <a href={'/'} className="text-3xl font-normal">
                         lazuwrite
                     </a>
-                <div className="">
-                {/* <a href={"/about"}>
-                    <span className="font-normal text-md hover:text-stone-400">me</span>
-                </a> */}
-                </div>
                 </div>
             </div>
     );
